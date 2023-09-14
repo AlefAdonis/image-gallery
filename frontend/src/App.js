793b5285d4e314b85e9db8295aa91e35e626670c
AlefAdonis <alefadonis10@gmail.com>
2023-09-14 11:43:17 -0300

    [add] Adding the request to the unsplash api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,24 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 const App = () => {
   const [wordSearch, setWordSearch] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(wordSearch);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${wordSearch}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (

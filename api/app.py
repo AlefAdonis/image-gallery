365d8d65ab77a4b1c7c1346e663150f501c35ea9
AlefAdonis <alefadonis10@gmail.com>
2023-11-09 00:14:05 -0300

    [add]Adding automatic debug detection

--- api/app.py
+++ api/app.py
@@ -9,11 +9,15 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_URL = "https://api.unsplash.com/photos/random/"
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("The token to Unslash API is missing!")
 
 app = Flask(__name__)
+
+app.config["DEBUG"] = DEBUG
+
 
 @app.route("/new-image")
 def new_image():

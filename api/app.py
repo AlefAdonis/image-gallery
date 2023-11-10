4f8fe389310ce642e291b19908b895f2607e438b
AlefAdonis <alefadonis10@gmail.com>
2023-11-10 11:15:29 -0300

    Enabled black code formatter

--- api/app.py
+++ api/app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 from flask import Flask, request
+from flask_cors import CORS
 
 
 # Loading the .env.local token to the enviroment
@@ -15,29 +16,23 @@ if not UNSPLASH_KEY:
     raise EnvironmentError("The token to Unslash API is missing!")
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["DEBUG"] = DEBUG
 
 
-@app.route("/new-image")
+    @app.route("/new-image")
 def new_image():
     word = request.args.get("query")
 
-    headers = {
-        "Authorization": f"Client-ID {UNSPLASH_KEY}",
-        "Accept-Version": "v1" 
-    }
+    headers = {"Authorization": f"Client-ID {UNSPLASH_KEY}", "Accept-Version": "v1"}
 
-    payload = {
-        "query": word
-    }
+    payload = {"query": word}
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=payload)
 
     data = response.json()
     return data
-    
 
-    
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
